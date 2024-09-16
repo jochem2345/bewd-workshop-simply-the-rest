fb77d51bda7be1357f377fc3a633c2b63f5bfe56
Jochem Jansen <jd.jansen2@student.han.nl>
2024-09-16 19:44:41 +0200

    Demo toegevoegd, niet de gemaakte versie

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	// Voeg deze code toe aan JeProjectApplication.java en
+	// roep hem aan via de url
+	// http://localhost:8080/hello?myName=superstudent
+	@GetMapping("/hello")
+	public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+}
